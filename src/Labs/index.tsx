6f4adf0d9cf833addf46ff063eb4d327ebfc2ead
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-09-11 12:55:59 -0400

    added git repo link

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,3 +1,4 @@
+// Julie Kim
 import Lab1 from "./Lab1"
 import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
@@ -6,7 +7,9 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
         <div id="wd-labs">
+            Julie Kim
             <h1>Labs</h1>
+            <a href="https://github.com/jjuliekim/kanbas-react-web-app.git" id="wd-github" target="_blank" rel="noreferrer" >Github Repository</a>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
