2231c01b8589df370725b1948c2ab5f3431f9d03
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-09-23 15:29:31 -0400

    a2 3.bootstrap

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -11,6 +11,12 @@ import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
 import ReactIconsSampler from "./ReactIcons";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
   return (
@@ -90,6 +96,18 @@ export default function Lab2() {
       <Flex />
 
       <ReactIconsSampler />
+
+      <BootstrapGrids />
+
+      <ScreenSizeLabel />
+
+      <BootstrapTables />
+
+      <BootstrapLists />
+
+      <BootstrapForms />
+
+      <BootstrapNavigation />
 
     </div>
   );
