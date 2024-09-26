493f1f132f6ffedd0cf5f29c5a34236e6e06d0bb
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-09-26 13:04:32 -0400

    a2 search bar

--- src/Kanbas/Courses/Assignments/AssignmentHeaderButtons.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentHeaderButtons.tsx
@@ -4,6 +4,7 @@ import { FaPlus } from "react-icons/fa6";
 export default function AssignmentHeaderButtons() {
     return (
         <div className="float-end">
+            40% of Total
             <FaPlus className="fs-4" />
             <IoEllipsisVertical className="fs-4" />
         </div>
--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -4,39 +4,53 @@ import AssignmentsControl from "./AssignmentsControl";
 import BSGripVertical from "../Modules/BsGripVertical";
 import { BsGripVertical } from "react-icons/bs";
 import { VscNotebook } from "react-icons/vsc";
-// import { IoIosSearch } from "react-icons/io";
+import { IoIosSearch } from "react-icons/io";
 
 export default function Assignments() {
     return (
         <div>
-            <div>
-                <input id="wd-search-assignment" 
-                    placeholder="Search for Assignments"/>
-                <AssignmentsControl /> <br /><br /><br /><br />
+            <div className="row mb-3 align-items-center">
+                <div className="col-auto">
+                    {/* <input id="wd-search-assignment" className="form-control"
+                        placeholder="Search for Assignments" style={{ width: "300px" }} /> */}
+                    <div className="input-group">
+                        <span className="input-group-text">
+                            <IoIosSearch />
+                        </span>
+                        <input
+                            id="wd-search-assignment"
+                            className="form-control"
+                            placeholder="Search for Assignments"
+                        />
+                    </div>
+                </div>
+                <div className="col-auto ms-auto">
+                    <AssignmentsControl />
+                </div>
             </div>
             <ul id="wd-assignments" className="list-group rounded-0">
                 <li className="wd-assignment-group list-group-item p-0 mb-5 fs-5 border-gray">
                     <div className="wd-title p-3 ps-2 bg-secondary">
                         <BSGripVertical />
-                        ASSIGNMENTS 40% of Total
+                        ASSIGNMENTS
                         <AssignmentHeaderButtons />
                     </div>
                     <ul className="wd-lessons list-group rounded-0">
                         <li className="wd-lesson list-group-item p-3 ps-1">
                             <BsGripVertical />
-                            <VscNotebook />
+                            <VscNotebook color="green" />
                             A1
                             <LessonControlButtons />
                         </li>
                         <li className="wd-lesson list-group-item p-3 ps-1">
                             <BsGripVertical />
-                            <VscNotebook />
-                            A2 
+                            <VscNotebook color="green" />
+                            A2
                             <LessonControlButtons />
                         </li>
                         <li className="wd-lesson list-group-item p-3 ps-1">
                             <BsGripVertical />
-                            <VscNotebook />
+                            <VscNotebook color="green" />
                             A3
                             <LessonControlButtons />
                         </li>
