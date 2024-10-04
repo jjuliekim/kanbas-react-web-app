d12dc0fb8e82f3b6a3e73794ed5c9ff6aeacece1
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-10-04 14:17:29 -0400

    a3 lab 2.3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,30 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+
 export default function Lab3() {
     return (
       <div>
         <h2>Lab 3</h2>
+        <VariablesAndConstants />
+        <VariableTypes />
+        <BooleanVariables />
+        <IfElse />
+        <TernaryOperator />
+        <ConditionalOutputIfElse />
+        <ConditionalOutputInline />
+        <LegacyFunctions />
+        <ArrowFunctions />
+        <ImpliedReturn />
+        <TemplateLiterals />
       </div>
     );
   }
