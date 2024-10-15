58a4304a4c41486d74eb2edfe2cc27ecdd1ea005
Julie Kim <118621382+jjuliekim@users.noreply.github.com>
2024-10-15 19:09:48 -0400

    course images

--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
                             <div className="card rounded-3 overflow-hidden">
                                 <Link to={`/Kanbas/Courses/${course._id}/Home`}
                                     className="wd-dashboard-course-link text-decoration-none text-dark" >
-                                    <img src={course.image} alt="course-img" width="100%" height={160} />
+                                    <img src={`images/${course._id}.png`} alt="course-img" width="100%" height={160} />
                                     <div className="card-body">
                                         <h5 className="wd-dashboard-course-title card-title">
                                             {course.name}
